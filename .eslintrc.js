afab716f20196d2e507333286115437398090c8d
sheerLi <m17805850283@163.com>
2021-09-01 14:28:19 +0800

    chore: 完善gitflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   env: {
--- src/pages/find/index.vue
+++ src/pages/find/index.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>发现页面</div>
+  <div>发现页面{{ count }}</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue';
 
 export default defineComponent({
   components: {},
-  setup() {},
+  setup() {
+    const count = ref(1);
+
+    onMounted(() => {
+      setTimeout(() => {
+        count.value = 2;
+      }, 1000);
+    });
+
+    return {
+      count,
+    };
+  },
 });
 </script>
