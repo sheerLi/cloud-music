f21daa25fecced3cffd7f4131dccba8cfe01755b
sheerLi <m17805850283@163.com>
2021-09-01 16:00:22 +0800

    feat: 修改axios库

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,48 +2,56 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import qs from 'qs';
 
 export default class Axios {
-  static request(config: AxiosRequestConfig) {
+  static request(config: AxiosRequestConfig): Promise<AxiosResponse<unknown>> {
     return axios.request(config);
   }
 
-  static get(url: string, data: any) {
+  static get(url: string, data: unknown): Promise<AxiosResponse<unknown>> {
     return axios.get(url, {
       params: data,
       paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }),
     });
   }
 
-  static post(url: string, data?: any, config?: AxiosRequestConfig) {
+  static post(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<unknown>> {
     return axios.post(url, data, config);
   }
 
-  static put(url: string, data?: any, config?: AxiosRequestConfig) {
+  static put(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<unknown>> {
     return axios.put(url, data, config);
   }
 
-  static delete(url: string, data?: any) {
+  static delete(url: string, data?: any): Promise<AxiosResponse<unknown>> {
     return axios.delete(url, data);
   }
 
-  static setBaseURL(url: string) {
+  static setBaseURL(url: string): void {
     axios.defaults.baseURL = url;
   }
 
-  static setHeader(key: string, value: any) {
+  static setHeader(key: string, value: any): void {
     axios.defaults.headers.common[key] = value;
   }
 
   static addRequestInterceptor(
     onFulfilled: (val: AxiosRequestConfig) => AxiosRequestConfig | Promise<AxiosRequestConfig>,
     onRejected: (err: any) => any
-  ) {
+  ): void {
     axios.interceptors.request.use(onFulfilled, onRejected);
   }
 
   static addResponseInterceptor(
     onFulfilled: (val: AxiosResponse<any>) => AxiosResponse<any> | Promise<AxiosResponse<any>>,
     onRejected: (err: any) => any
-  ) {
+  ): void {
     axios.interceptors.response.use(onFulfilled, onRejected);
   }
 }
